221e8dc177d24ac9dd6d7e1467e3ca7077424c75
Aldiyar <aimamadiev@mail.ru>
2023-03-01 14:20:22 +0700

    2 Компоненты, свойства и JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+/* import App from './App';
+ */
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Welcome(props) {
+  console.log(props)
+  return <h1 style={{ color: props.color }}> Привет, {props.name}</h1 >
+}
+
+const h1Elem = <Welcome name='Messi' color='red' />
+
+root.render(
+  h1Elem
+);
